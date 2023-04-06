ab82e07b2a5a682d02f8fc8e3c1f11b308d9c4f0
ayaOaya <alyaauaacn@gmail.com>
2023-04-06 03:16:53 +0200

    refactor

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const usersController = require('../controllers/usersController')
+const notesController = require('../controllers/notesController')
 const verifyJWT = require('../middleware/verifyJWT')
 
 router.use(verifyJWT) //this will apply jwt middleware to all routes inside of this file
 
-
 router.route('/')
-    .get(usersController.getAllUsers)
-    .post(usersController.createNewUser)
-    .patch(usersController.updateUser)
-    .delete(usersController.deleteUser)
+    .get(notesController.getAllNotes)
+    .post(notesController.createNewNote)
+    .patch(notesController.updateNote)
+    .delete(notesController.deleteNote)
 
 module.exports = router
